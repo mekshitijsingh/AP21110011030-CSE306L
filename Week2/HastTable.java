e025495fc0f0f18a0f03967caa4f05e89f152356
Kshitij Singh <91209593+mekshitijsingh@users.noreply.github.com>
2023-08-30 00:01:31 +0530

    Update HastTable.java

--- Week2/HastTable.java
+++ Week2/HastTable.java
@@ -1,39 +1,35 @@
-import java.util.Objects;
+import java.util.Scanner;
 
-class HashTable {
-    private final int size;
-    private final Object[] table;
-
-    public HashTable(int size) {
-        this.size = size;
-        this.table = new Object[size];
-    }
-
-    private int hashFunction(String key) {
-        return Objects.hash(key) % size;
-    }
-
-    public void insert(String key, int value) {
-        int index = hashFunction(key);
-        table[index] = value;
-    }
-
-    public Integer get(String key) {
-        int index = hashFunction(key);
-        if (table[index] != null) {
-            return (Integer) table[index];
-        }
-        return null;
-    }
-
-    public void delete(String key) {
-        int index = hashFunction(key);
-        table[index] = null;
-    }
-
+public class HashTable {
     public static void main(String[] args) {
-        HashTable symbolTable = new HashTable(100);
-        symbolTable.insert("variable1", 42);
-        System.out.println(symbolTable.get("variable1")); // Output: 42
+        int n, i = 0, j = 0;
+        char[] id_Array2 = new char[20];
+        char ch;
+
+        System.out.println("Input the expression ending with ; sign:");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        while (i < input.length() && input.charAt(i) != ';') {
+            id_Array2[i] = input.charAt(i);
+            i++;
+        }
+        n = i - 1;
+        System.out.println("\nSymbol Table display");
+        System.out.println("Symbol \t addr \t\t\t type");
+
+        while (j <= n) {
+            ch = id_Array2[j];
+
+            if (Character.isLetter(ch)) {
+                System.out.println("\n " + ch + " \t " + System.identityHashCode(ch) + " \t identifier");
+                j++;
+            } else {
+                if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == '=' || ch == '<' || ch == '>') {
+                    System.out.println("\n " + ch + " \t " + System.identityHashCode(ch) + " \t operator");
+                    j++;
+                }
+            }
+        }
     }
 }
