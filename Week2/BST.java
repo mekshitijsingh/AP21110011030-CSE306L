9132f1562c8738cda2cb46fdf1c9e6804405346e
Kshitij Singh <91209593+mekshitijsingh@users.noreply.github.com>
2023-08-29 23:11:31 +0530

    Update BST.java

--- Week2/BST.java
+++ Week2/BST.java
@@ -1,53 +1,35 @@
-class TreeNode {
-    String key;
-    int value;
-    TreeNode left;
-    TreeNode right;
+import java.util.Scanner;
 
-    public TreeNode(String key, int value) {
-        this.key = key;
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-class BinarySearchTree {
-    TreeNode root;
-
-    public void insert(String key, int value) {
-        root = insertRecursive(root, key, value);
-    }
-
-    private TreeNode insertRecursive(TreeNode current, String key, int value) {
-        if (current == null) {
-            return new TreeNode(key, value);
-        }
-        if (key.compareTo(current.key) < 0) {
-            current.left = insertRecursive(current.left, key, value);
-        } else if (key.compareTo(current.key) > 0) {
-            current.right = insertRecursive(current.right, key, value);
-        }
-        return current;
-    }
-
-    public Integer search(String key) {
-        return searchRecursive(root, key);
-    }
-
-    private Integer searchRecursive(TreeNode current, String key) {
-        if (current == null || current.key.equals(key)) {
-            return current != null ? current.value : null;
-        }
-        if (key.compareTo(current.key) < 0) {
-            return searchRecursive(current.left, key);
-        }
-        return searchRecursive(current.right, key);
-    }
-
+public class BinarySearchTree {
     public static void main(String[] args) {
-        BinarySearchTree symbolTable = new BinarySearchTree();
-        symbolTable.insert("variable1", 42);
-        System.out.println(symbolTable.search("variable1")); // Output: 42
+        int n, i = 0, j = 0;
+        char[] id_Array2 = new char[20];
+        char ch;
+
+        System.out.println("Input the expression ending with ; sign:");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        while (i < input.length() && input.charAt(i) != ';') {
+            id_Array2[i] = input.charAt(i);
+            i++;
+        }
+        n = i - 1;
+        System.out.println("\nSymbol Table display");
+        System.out.println("Symbol \t addr \t\t\t type");
+
+        while (j <= n) {
+            ch = id_Array2[j];
+
+            if (Character.isLetter(ch)) {
+                System.out.println("\n " + ch + " \t " + System.identityHashCode(ch) + " \t identifier");
+                j++;
+            } else {
+                if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == '=' || ch == '<' || ch == '>') {
+                    System.out.println("\n " + ch + " \t " + System.identityHashCode(ch) + " \t operator");
+                    j++;
+                }
+            }
+        }
     }
 }
